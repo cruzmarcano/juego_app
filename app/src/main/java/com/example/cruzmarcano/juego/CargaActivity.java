876e03d1c882c1fd8840f4dea53d72a8e8d739b6
cruzmarcano <cruzmarcano@gmail.com>
2018-01-17 02:53:27 -0400

    Initial commit

---
+++ app/src/main/java/com/example/cruzmarcano/juego/CargaActivity.java
@@ -0,0 +1,45 @@
+package com.example.cruzmarcano.juego;
+
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.cruzmarcano.juego.datos.BDalzheimer;
+
+public class CargaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carga);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(CargaActivity.this, MainActivity.class);
+                startActivity(intent);
+
+                //se crea la base de datos
+                BDalzheimer tablas = new BDalzheimer(CargaActivity.this,"Alzhaimer",  null,1 );
+
+                //ponemos en modo escritura
+                SQLiteDatabase db =  tablas.getWritableDatabase();
+
+                //si se creo la bd por primera vez
+                // le colocamos los datos de la plantilla
+                if (db != null){
+                    db.execSQL("INSERT INTO plantilla(planti_id, planti_nombre) VALUES (null, 'cartas');");
+                    db.execSQL("INSERT INTO recurso(recur_id, recur_nomb) VALUES (null, 'cocina.jpg');");
+                    db.execSQL("INSERT INTO grupo(grupo_id, grupo_nomb, grupo_color, grupo_imag) VALUES (null, 'familia','verde',1);");
+                    db.execSQL("INSERT INTO juego(juego_id, planti_fk, juego_nomb, juego_instr,\n" +
+                               "\t juego_pre_resp,juego_pre_resp2,juego_pre_resp3,juego_pre_resp4) VALUES (null, 1,\n" +
+                               "\t'cartas_1','seleccione un carta',1,1,1,1);");
+                    db.execSQL("INSERT INTO jue_grup(jue_grup, juego_fk, grupo_fk) VALUES (null, 1,1);");
+
+                }
+            }
+        },2000);
+    }
+}
---
+++ app/src/main/java/com/example/cruzmarcano/juego/datos/BDalzheimer.java
@@ -0,0 +1,74 @@
+package com.example.cruzmarcano.juego.datos;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by cruzmarcano on 11/1/18.
+ */
+
+public class BDalzheimer extends SQLiteOpenHelper {
+
+    //tabla grupo query
+    String grupo = "CREATE TABLE `grupo` (\n" +
+            "\t`grupo_id`\tINTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,\n" +
+            "\t`grupo_nomb`\tTEXT NOT NULL,`grupo_color`\tTEXT NOT NULL,\n" +
+            "\t`grupo_imag`\tINTEGER NOT NULL,\n" +
+            "\tFOREIGN KEY(`grupo_imag`) REFERENCES `recurso`(`recur_id`))";
+
+    //tabla jueg_grup query
+    String jue_grup="CREATE TABLE `jue_grup` (\n" +
+            "\t`jue_grup`\tINTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,\n" +
+            "\t`juego_fk`\tINTEGER NOT NULL,\n" +
+            "\t`grupo_fk`\tINTEGER NOT NULL,\n" +
+            "\tFOREIGN KEY(`grupo_fk`) REFERENCES `grupo`(`grupo_id`),\n" +
+            "\tFOREIGN KEY(`juego_fk`) REFERENCES `juego`(`juego_id`))";
+
+    //tabla juego query
+    String juego = "CREATE TABLE `juego` (\n" +
+            "\t`juego_id`\tINTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,\n" +
+            "\t`planti_fk`\tINTEGER NOT NULL,\n" +
+            "\t`juego_nomb`\tTEXT NOT NULL,\n" +
+            "\t`juego_instr`\tTEXT NOT NULL,\n" +
+            "\t`juego_pre_resp`\tINTEGER NOT NULL,\n" +
+            "\t`juego_pre_resp2`\tINTEGER NOT NULL,\n" +
+            "\t`juego_pre_resp3`\tINTEGER NOT NULL,\n" +
+            "\t`juego_pre_resp4`\tINTEGER NOT NULL,\n" +
+            "\tFOREIGN KEY(`planti_fk`) REFERENCES `platilla`(`planti_id`),\n" +
+            "\tFOREIGN KEY(`juego_pre_resp3`) REFERENCES `recurso`(`recur_id`),\n" +
+            "\tFOREIGN KEY(`juego_pre_resp4`) REFERENCES `recurso`(`recur_id`),\n" +
+            "\tFOREIGN KEY(`juego_pre_resp`) REFERENCES `recurso`(`recur_id`),\n" +
+            "\tFOREIGN KEY(`juego_pre_resp2`) REFERENCES `recurso`(`recur_id`))";
+
+    //tabla plantilla query
+    String plantilla = "CREATE TABLE `plantilla` (\n" +
+            "\t`planti_id`\tINTEGER PRIMARY KEY AUTOINCREMENT,\n" +
+            "\t`planti_nombre`\tTEXT NOT NULL)";
+
+    //tabla recurso query
+    String recurso = "CREATE TABLE `recurso` (\n" +
+            "\t`recur_id`\tINTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,\n" +
+            "\t`recur_nomb`\tTEXT NOT NULL)";
+
+    public BDalzheimer(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+    // si no existe la base de datos la creamos ejecutando los querys
+        db.execSQL(plantilla);
+        db.execSQL(recurso);
+        db.execSQL(grupo);
+        db.execSQL(juego);
+        db.execSQL(jue_grup);
+
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+
+    }
+}
