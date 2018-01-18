675a44002c3c2d2d3c4212bd312f40ae6f03ace0
cruzmarcano <cruzmarcano@gmail.com>
2018-01-18 16:19:15 -0400

    menu imagenes sencillo

--- app/src/main/java/com/example/cruzmarcano/juego/MainActivity.java
+++ app/src/main/java/com/example/cruzmarcano/juego/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.cruzmarcano.juego;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.TabHost;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar= (Toolbar) findViewById(R.id.barra);
         setSupportActionBar(toolbar);
+
+
     }
 
 
