93924b2f92096b6732aa8594aacc635ead34d449
cruzmarcano <cruzmarcano@gmail.com>
2018-01-18 23:52:21 -0400

    clase validador importada

--- app/src/main/java/com/example/cruzmarcano/juego/MemoriaActivity.java
+++ app/src/main/java/com/example/cruzmarcano/juego/MemoriaActivity.java
@@ -5,13 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 public class MemoriaActivity extends AppCompatActivity {
 
-    ImageButton memoria, lenguaje, orientacion, atencion, visual ;
+    ImageButton memoria, lenguaje, orientacion, atencion, visual, memo_ima,memo_sonido1,memo_sonido2,memo_sonido3;
+    Button guardar;
+    EditText  nombre, instrucion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,18 +24,22 @@ public class MemoriaActivity extends AppCompatActivity {
 
         Toolbar toolbar= (Toolbar) findViewById(R.id.barra);
         setSupportActionBar(toolbar);
+        //cateamos todos los objetos que estan en la plantilla
+        //texto de nombre e instruccion del juego
+        nombre=(EditText) findViewById(R.id.memo_jueg_nomb);
+        instrucion=(EditText) findViewById(R.id.memo_jue_inst);
+        //imagen
+        memo_ima = (ImageButton) findViewById(R.id.memo_ima);
+        //sonido
+        memo_sonido1 = (ImageButton) findViewById(R.id.btn_sonid_1);
+        memo_sonido2 = (ImageButton) findViewById(R.id.btn_sonid_2);
+        memo_sonido3 = (ImageButton) findViewById(R.id.btn_sonid_3);
 
-        /*boton menu memoria
-        memoria =(ImageButton)findViewById(R.id.memo_btn);
-        memoria.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Toast.makeText(MemoriaActivity.this,"memoria",Toast.LENGTH_LONG).show();
-                //Intent atencion =new Intent(MemoriaActivity.this,AtencionActivity.class);
-                //startActivity(atencion);
+        guardar=(Button)findViewById(R.id.memo_guar_btn);
+        //numero de la plantilla
+        int plantilla =1;
 
-            }
-        });*/
+
         //boton lenguaje
         lenguaje =(ImageButton)findViewById(R.id.leng_btn);
         lenguaje.setOnClickListener(new View.OnClickListener() {
