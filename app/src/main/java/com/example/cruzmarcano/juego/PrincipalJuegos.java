a0db266c10378fb4da9500cf6c86241b82fe7852
cruz marcano <cruzmarcano@gmail.com>
2018-03-04 20:09:39 -0400

    Tabs funcionan con boton

--- app/src/main/java/com/example/cruzmarcano/juego/PrincipalJuegos.java
+++ app/src/main/java/com/example/cruzmarcano/juego/PrincipalJuegos.java
@@ -1,6 +1,7 @@
 package com.example.cruzmarcano.juego;
 
 
+import android.annotation.SuppressLint;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -8,23 +9,61 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import me.kaelaela.verticalviewpager.transforms.DefaultTransformer;
 
 
-public class PrincipalJuegos extends AppCompatActivity {
-
+public class PrincipalJuegos extends AppCompatActivity implements View.OnClickListener {
+    ImageButton memoria, atencion, lenguage,visual, orientacion;
+    ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.principal_juegos);
 
-        TabLayout tabLayout =(TabLayout)findViewById(R.id.tabJuego);
-        ViewPager viewPager =(ViewPager)findViewById(R.id.paginaJuego);
+        viewPager =(ViewPager)findViewById(R.id.paginaJuego);
 
         viewPager.setAdapter(new AdaptadorDePaginas (getSupportFragmentManager()));
+        memoria=(ImageButton)findViewById(R.id.memoBtn);
+        lenguage=(ImageButton)findViewById(R.id.lengBtn);
+        atencion=(ImageButton)findViewById(R.id.atenBtn);
+        orientacion=(ImageButton)findViewById(R.id.orienBtn);
+        visual=(ImageButton)findViewById(R.id.visualBtn);
 
-        tabLayout.setupWithViewPager(viewPager);
+        memoria.setOnClickListener(this);
+        lenguage.setOnClickListener(this);
+        atencion.setOnClickListener(this);
+        orientacion.setOnClickListener(this);
+        visual.setOnClickListener(this);
+
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.memoBtn:
+                viewPager.setCurrentItem(0);
+                break;
+            case R.id.lengBtn:
+                viewPager.setCurrentItem(1);
+                break;
+            case R.id.atenBtn:
+                viewPager.setCurrentItem(2);
+                break;
+            case R.id.orienBtn:
+                viewPager.setCurrentItem(3);
+
+                break;
+            case R.id.visualBtn:
+
+                viewPager.setCurrentItem(4);
+                break;
+        }
     }
 
     //creamos un metodo que es el que permite llenar el viewPager
@@ -32,7 +71,6 @@ public class PrincipalJuegos extends AppCompatActivity {
     public class AdaptadorDePaginas extends FragmentPagerAdapter {
         //nos pide un constructor
         public AdaptadorDePaginas(FragmentManager fm) {
-
             super(fm);
 
         }
@@ -40,6 +78,7 @@ public class PrincipalJuegos extends AppCompatActivity {
         //este metodo identifica los fragmen mostrados
         @Override
         public Fragment getItem(int position) {
+
             switch (position){
                 case 0:
                     return new PrincipalJuegosA();
@@ -60,26 +99,6 @@ public class PrincipalJuegos extends AppCompatActivity {
         @Override
         public int getCount() {
             return 5;
-        }
-        //sobre escribimos el metodo getPageTitle para que muestre el titulo de los botones del tab
-        @Override
-        public CharSequence getPageTitle(int position){
-
-            switch (position){
-                case 0:
-                    return "Memoria";
-                case 1:
-                    return "Lenguaje";
-                case 2:
-                    return "Orientacion";
-                case 3:
-                    return "Atencion";
-                case 4:
-                    return "Visual";
-                default:
-                    return "Memoria";
-            }
-
         }
 
     }
